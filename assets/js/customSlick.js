39d665d3defc84b4e13bcdeaf2559a7deaae7fed
usama1s <usama_1s@hotmail.com>
2021-10-30 18:01:55 +0500

    first commit

--- assets/js/customSlick.js
+++ assets/js/customSlick.js
@@ -7,7 +7,6 @@ $(document).ready(function() {
     arrows: true,
     prevArrow: '<div class="slick-prev"><i class="fas fa-chevron-left"></i></div>',
     nextArrow: '<div class="slick-next"><i class="fas fa-chevron-right"></i></div>',
-slidesToShow: 1,
 dots: true,
 infinite: true,
 slidesToShow: 3,
@@ -15,4 +14,22 @@ slidesToShow: 1,
   centerMode: true,
   variableWidth: true
   });
+
+
+
+ //  $('.prize-slider').slick({
+ //    arrows: true,
+ //    prevArrow: '<div class="slick-prev"><i class="fas fa-chevron-left"></i></div>',
+ //    nextArrow: '<div class="slick-next"><i class="fas fa-chevron-right"></i></div>',
+ //    draggable: true,
+ //
+ // centerMode: true,
+ // // variableWidth: true,
+ // slidesToShow: 1,
+ // arrows: true,
+ // dots: true,
+ // infinite: false
+ //  });
+
+
 });
